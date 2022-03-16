098d682c33c67d09a3fe56f226eb69de72b11145
Aidan Amato <aidanamato@comcast.net>
2022-03-16 15:33:48 -0400

    add ability to set type of new reimbursement in request

--- src/main/java/com/revature/foundation/dtos/requests/ReimbursementRequest.java
+++ src/main/java/com/revature/foundation/dtos/requests/ReimbursementRequest.java
@@ -5,12 +5,14 @@ import com.revature.foundation.models.Reimbursement;
 public class ReimbursementRequest {
     private float amount;
     private String description;
+    private String type;
 
     public ReimbursementRequest(){ super();}
 
-    public ReimbursementRequest(float amount, String description){
+    public ReimbursementRequest(float amount, String description, String type){
         this.amount = amount;
         this.description = description;
+        this.type = type.toUpperCase();
     }
 
     public double getAmount(){return amount;}
@@ -21,16 +23,24 @@ public class ReimbursementRequest {
 
     public void setDescription(String description){this.description=description;}
 
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type.toUpperCase();
+    }
 
     public Reimbursement extractReimbursement() {
         return new Reimbursement(amount, description);
     }
 
+    @Override
     public String toString() {
         return "ReimbursementRequest{" +
-                "amount='" + amount + '\'' +
+                "amount=" + amount +
                 ", description='" + description + '\'' +
+                ", type='" + type + '\'' +
                 '}';
     }
-
 }
