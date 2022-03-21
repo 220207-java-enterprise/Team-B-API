d36f7e4d791a54a4131e3dab2560dbe8f2613c9d
Aidan Amato <aidanamato@comcast.net>
2022-03-21 19:16:57 -0400

    added swagger ui and actuator

--- src/main/java/com/revature/ers/AppDriver.java
+++ src/main/java/com/revature/ers/AppDriver.java
@@ -3,19 +3,11 @@ package com.revature.ers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 public class AppDriver {
     public static void main(String[] args) {
         SpringApplication.run(AppDriver.class);
-
-        @Bean
-        public Docket api() {
-            return new Docket(DocumentationType.SWAGGER_2)
-                    .select()
-                    .apis(RequestHandlerSelectors.any())
-                    .paths(PathSelectors.any())
-                    .build();
-        }
     }
 }
