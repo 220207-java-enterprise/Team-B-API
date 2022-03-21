6dcd7761cff7d0c9e8ec06f9b8d9c904684da30b
Aidan Amato <aidanamato@comcast.net>
2022-03-21 18:14:40 -0400

    add springfox and swagger dependencies

--- src/main/java/com/revature/ers/AppDriver.java
+++ src/main/java/com/revature/ers/AppDriver.java
@@ -2,10 +2,20 @@ package com.revature.ers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AppDriver {
     public static void main(String[] args) {
         SpringApplication.run(AppDriver.class);
+
+        @Bean
+        public Docket api() {
+            return new Docket(DocumentationType.SWAGGER_2)
+                    .select()
+                    .apis(RequestHandlerSelectors.any())
+                    .paths(PathSelectors.any())
+                    .build();
+        }
     }
 }
